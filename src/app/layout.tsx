3b6b10a3cf0e62cda4162f3ee9115d0f9eb753b3
Octavio Vera <ofvera@uc.cl>
2025-07-07 00:03:39 -0400

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Vercel Next.js Template',
+  title: 'Vercel Next.js Templaten',
   description: 'Hello World Next.js App',
 }
 
